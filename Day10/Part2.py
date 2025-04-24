9eb547959843d4d667df5770c703dbc480fd164a
Bradley Dufour <dufour.bradley@gmail.com>
2025-04-24 17:16:00 -0400

    Day 10

---
+++ Day10/Part2.py
@@ -0,0 +1,33 @@
+import copy
+import queue
+
+file = open("input")
+
+map = []
+trailheads = []
+for lineNum,line in enumerate(file):
+    map.append([int(x) for x in line[:-1]])
+    trailheads.extend([[lineNum,enum] for enum,x in enumerate(line) if x == '0'])
+
+def getMapValueAt(map, position):
+    return map[position[0]][position[1]]
+
+movements = [lambda coord : [coord[0]+1,coord[1]], lambda coord : [coord[0]-1,coord[1]], lambda coord : [coord[0],coord[1]+1], lambda coord : [coord[0],coord[1]-1]]
+trailheadScore = 0
+for trailhead in trailheads:
+    tempMap = copy.deepcopy(map)
+    possiblePositions = queue.Queue()
+    possiblePositions.put(trailhead)
+    while not possiblePositions.empty():
+        curPos = possiblePositions.get()
+        print(curPos)
+        if(getMapValueAt(tempMap, curPos) == 9):
+            trailheadScore += 1
+        for operation in movements:
+            nextPos = operation(curPos)
+            if(nextPos[0] >= 0 and nextPos[0] < len(map) and nextPos[1] >= 0 and nextPos[1] < len(map[0]) and
+                getMapValueAt(tempMap, nextPos) - getMapValueAt(tempMap, curPos) == 1):
+                possiblePositions.put(nextPos)
+                
+
+print(trailheadScore)    
